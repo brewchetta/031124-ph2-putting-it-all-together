ef475feda5af4cf4693eac808459a5f31a68fca0
brewchetta <chett.tiller@gmail.com>
2024-04-08 09:33:06 -0400

    init

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <div className="App">
+
+      <h1>Putting It All Together</h1>
+
+      <p>Hmm... seems kind of empty so far...</p>
+
+    </div>
+  )
+}
+
+export default App
